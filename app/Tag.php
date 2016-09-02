18f9e3d07ea09606da7d48bb529c88c17f0aa254
jeremypaulbruce <jeremy.paul.bruce@gmail.com>
2016-09-03 11:42:35 +1200

    New Tag Model

---
+++ app/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+//use Jenssegers\Mongodb\Eloquent\Model;
+
+class Tag extends Model
+{
+    //protected $connection = 'mongodb';
+
+
+}
